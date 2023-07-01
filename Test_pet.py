ad7dfb8174c5e618cdca571c0ecc4795cf46f27f
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 20:05:01 +0800

    Pseudocode

---
+++ Test_pet.py
@@ -0,0 +1,11 @@
+# Import Pet class
+
+# Create class for PetTest
+    # Create object for the Pet class
+
+    # Prompt user to enter pet details
+
+    # Display pet details
+
+# Create instance for PetTest class
+# Implement
