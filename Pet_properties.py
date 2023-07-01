74536d766648ff17cd3cdc5f56ffe0f71b2b3e90
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 20:01:06 +0800

    Add the methods

--- Pet_properties.py
+++ Pet_properties.py
@@ -8,10 +8,26 @@ class Pet:
 
 # Create mutator methods
     # Name
+    def set_name(self, name):
+        self.__name = name
+
     # Type
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
     # Age
+    def set_age(self, age):
+        self.__age = age
 
 # Create accessor methods
     # Name
+    def get_name(self):
+        return self.__name 
+
     # Type
+    def tet_animal_type(self):
+        return self.__animal_type
+    
     # Age
+    def get_age(self):
+        return self.__age
