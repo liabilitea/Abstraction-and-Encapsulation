9ec00d4a1de1b00d448be43f3161db9e4a758b8e
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 15:39:07 +0800

    Print properties of the two fans

--- Test_fan.py
+++ Test_fan.py
@@ -7,9 +7,20 @@ class TestFan:
     # Create two fans
         fan1 = Fan(Fan.FAST, True, 10, "Yellow")
         fan2 = Fan(Fan.MEDIUM, False, 5, "Blue")
+
 # Print properties of first fan
+        print("1st Fan's Properties:")
+        print("Speed:", fan1.get_speed)
+        print("Switch:", fan1.get_switch)
+        print("Radius:", fan1.get_radius)
+        print("Color:", fan1.get_color)
 
 # Print properties of second fan
-
+        print("2nd Fan's Properties:")
+        print("Speed:", fan2.get_speed)
+        print("Switch:", fan2.get_switch)
+        print("Radius:", fan2.get_radius)
+        print("Color:", fan2.get_color)
+        
 # Create instance of TestFan 
 # Run with run method
