34d91dbd201ecbdc04e7d08e5ad5ad9d54105171
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 18:25:21 +0800

    Add line for iteration

--- Test_car.py
+++ Test_car.py
@@ -16,18 +16,20 @@ class CarTest:
         header_color = colored(speed_header, color = 'light_red')
     # Print header
         print(header_color)
-        
+
     # Loop to accelerate and break five times
         for i in range(5):
             self.car.accelerate()
             print("Current speed:", self.car.get_speed())
             # Add line each iteration for clean output
+            print()
 
         for i in range(5):
             self.car.brake()
             print("Current speed:", self.car.get_speed())
             # Add line each iteration for clean output
-
+            print()
+            
 # Create an instance for CarTest
 car_test = CarTest()
 # Implement 
