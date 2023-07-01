f5f690b7998870055501d3d8e51c9c625593471b
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 20:14:38 +0800

    Get user input and print

--- Test_pet.py
+++ Test_pet.py
@@ -9,8 +9,21 @@ class PetTest:
         pet = Pet()
         
     # Prompt user to enter pet details
+        name = input("Please enter the name of your pet: ")
+        pet.set_name(name)
 
+        animal_type = input("Please enter what type of animal your pet is: ")
+        pet.set_animal_type(animal_type)
+    
+        age = input("Please enter the current age of your pet: ")
+        pet.set_age(age)
+    
     # Display pet details
+        print("\nPet Details:")
+        print("Name: ", (pet.get_name))
+        print("Type: ", (pet.get_animal_type))
+        print("Age: ", (pet.get_age))
+
 
 # Create instance for PetTest class
 # Implement
