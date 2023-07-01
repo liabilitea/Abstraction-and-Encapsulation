4cb629e8aedfe5c4c742fe32ffaae0b7d3521cc9
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 20:08:21 +0800

    Define PetTest class

--- Test_pet.py
+++ Test_pet.py
@@ -2,10 +2,12 @@
 from Pet_properties import Pet
 
 # Create class for PetTest
-
+class PetTest:
     # Create method to run the display
+    def implement(self):
     # Create object for the Pet class
-
+        pet = Pet()
+        
     # Prompt user to enter pet details
 
     # Display pet details
