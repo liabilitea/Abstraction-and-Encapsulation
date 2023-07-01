a2ce7858ca6e97198610b797fe8e9ff3b32b37d2
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 15:00:56 +0800

    Class and constructor

--- Fan_properties.py
+++ Fan_properties.py
@@ -1,8 +1,14 @@
 # Create Fan class
+class Fan:
 
     # Create a constructor, set as default
+    def __init__(self, speed=1, On=False, radius=5, color='Blue'):
     # All properties should be private
-
+        self.__speed = speed
+        self.__on = On
+        self.__radius = radius
+        self.__colro = color
+        
 # Define speed property
 
 # Create accessor and mutator methods to get and modify values
