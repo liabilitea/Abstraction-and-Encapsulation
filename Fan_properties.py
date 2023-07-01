ba78ae584097fbe5e92110a7f376e25f4261d5f7
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 14:51:51 +0800

    Pseudocode

---
+++ Fan_properties.py
@@ -0,0 +1,13 @@
+# Create Fan class
+
+    # Create a constructor, set as default
+    # All properties should be private
+
+# Define speed property
+
+# Create accessor and mutator methods to get and modify values
+    # Speed
+    # Switch, on and off
+    # Radius
+    # Color
+
