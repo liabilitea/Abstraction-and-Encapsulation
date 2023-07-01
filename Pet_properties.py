dba1a83a9ca9a0b0c2529c3d59ac5e068c92482a
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 19:54:59 +0800

    Pseudocode

---
+++ Pet_properties.py
@@ -0,0 +1,12 @@
+# Create class named Pet
+    # Create constructor to initialize attributes
+
+# Create mutator methods
+    # Name
+    # Type
+    # Age
+
+# Create accessor methods
+    # Name
+    # Type
+    # Age
