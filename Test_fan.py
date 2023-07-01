f2649bdb076708673e3cf878361038148833dd57
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 15:27:01 +0800

    Pseudocode

---
+++ Test_fan.py
@@ -0,0 +1,11 @@
+# Import Fan class from Fan_properties
+
+# Create class for TestFan
+    # Create two fans
+
+# Print properties of first fan
+
+# Print properties of second fan
+
+# Create instance of TestFan 
+# Run with run method
