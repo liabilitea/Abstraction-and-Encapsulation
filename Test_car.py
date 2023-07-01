e000e11ff3f0bfea626da63f6ba8d39ae43155fc
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 18:09:43 +0800

    Define CarTest class

--- Test_car.py
+++ Test_car.py
@@ -1,8 +1,15 @@
 # Import Car class
+from Car_properties import Car
 
 # Create class for testing 
+class CarTest:
+    def __init__(self):
+        self.car = Car(1967, "Volkswagen")
 
-# Loop to accelerate and break five times
+
+    # Loop to accelerate and break five times
+
+
 
 # Create an instance for CarTest
 # Implement 
