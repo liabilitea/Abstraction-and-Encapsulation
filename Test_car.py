cb9de8761900841c0c299afc475626f3c272719a
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 18:11:26 +0800

    Create methods

--- Test_car.py
+++ Test_car.py
@@ -6,10 +6,15 @@ class CarTest:
     def __init__(self):
         self.car = Car(1967, "Volkswagen")
 
-
+    def implement(self):
     # Loop to accelerate and break five times
+        for i in range(5):
+            self.car.accelerate()
+            print("Current speed:", self.car.get_speed())
 
-
+        for i in range(5):
+            self.car.brake()
+            print("Current speed:", self.car.get_brake())
 
 # Create an instance for CarTest
 # Implement 
