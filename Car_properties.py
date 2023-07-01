6730c6836d71cb88071542392c4f4726b3887110
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 17:47:58 +0800

    Pseudocode

---
+++ Car_properties.py
@@ -0,0 +1,10 @@
+# Create a class named Car
+
+    # Create a constructor to initialize Car's attributes
+
+    # Create methods
+        # Accelerate +5
+        # Brake -5
+        # Current Speed
+
+
