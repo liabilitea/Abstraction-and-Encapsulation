244c7be6706005c5213c9723babf285b05c879a8
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 20:06:33 +0800

    Import class Pet

--- Test_pet.py
+++ Test_pet.py
@@ -1,6 +1,9 @@
 # Import Pet class
+from Pet_properties import Pet
 
 # Create class for PetTest
+
+    # Create method to run the display
     # Create object for the Pet class
 
     # Prompt user to enter pet details
