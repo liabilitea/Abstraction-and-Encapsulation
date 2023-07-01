25d5ecb67277edab1895db8ddcd304aa9542c805
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 15:32:00 +0800

    Import and class definition

--- Test_fan.py
+++ Test_fan.py
@@ -1,8 +1,12 @@
 # Import Fan class from Fan_properties
+from Fan_properties import Fan
 
 # Create class for TestFan
+class TestFan:
+    def implement(self):
     # Create two fans
-
+        fan1 = Fan(Fan.FAST, True, 10, "Yellow")
+        fan1 = Fan(Fan.FAST, True, 10, "Yellow")
 # Print properties of first fan
 
 # Print properties of second fan
