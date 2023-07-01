35f15c4495af22115588b570be3a4e2dac834135
Frances Leigh <fleighbaay@gmail.com>
2023-07-01 17:50:31 +0800

    Define class, add attributes

--- Car_properties.py
+++ Car_properties.py
@@ -1,7 +1,11 @@
 # Create a class named Car
-
+class Car:
     # Create a constructor to initialize Car's attributes
-
+    def __init__(self, year, make):
+        self.__year = year
+        self.__make = make
+        self.__speed = 0
+        
     # Create methods
         # Accelerate +5
         # Brake -5
